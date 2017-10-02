5f8955e0e6fb572fb303ae28d1eb3066af878335
LinJiaB00755804 <31933925+LinJiaB00755804@users.noreply.github.com>
2017-10-02 16:08:29 -0300

    second commit

--- task1.py
+++ task1.py
@@ -1,24 +1,24 @@
 import string
 with open('Crime.csv','r') as fin:
     mylist = []
-    mydic = {}
+    mydic1 = {}
+    mydic2 = {}
     for line in fin:
         clean_line = line.strip(string.whitespace)
         new_clean_line = clean_line.strip()
-        wordlist = new_clean_line.split()
-        if len(wordlist)>3:
-            s = wordlist[-3].split()
-            d = wordlist[-2]
-        if wordlist[-1] in mydic:
-            mydic[wordlist[-1]] +=1
+        wordlist = new_clean_line.split(',')
+        mydic2[wordlist[-1]]=wordlist[-2]
+        if wordlist[-1] in mydic1:
+            mydic1[wordlist[-1]] +=1
         else:
-            mydic[wordlist[-1]] = 1
-    for count,name in sorted([ (value,key) for (key,value) in mydic.items()], reverse = True):
-        if ',' in name:
-            t = name.split()
-            print(t[-1],t[-2],count)
-        elif ',' in wordlist[-3]:
-            print(s[2],d,name,s[1],count)
+            mydic1[wordlist[-1]] = 1
+    for count,name in sorted([ (value,key) for (key,value) in mydic1.items()], reverse = True):
+        mylist.append(name)
+        mylist.append(mydic2[name])
+        mylist.append(count)
+        
+    print(mylist)
+            
     
 
-    
+
